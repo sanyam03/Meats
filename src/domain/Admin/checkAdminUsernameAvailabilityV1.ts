8ddbff69e0b3e0271943fb94ff2ec430edb8d4e3
Kunal Gosrani <iamkunalgosrani@gmail.com>
2022-05-31 15:14:54 +0530

    test: admin/login/v1 API

---
+++ src/domain/Admin/checkAdminUsernameAvailabilityV1.ts
@@ -0,0 +1,16 @@
+import { createDatabaseSession, DatabaseSession } from "@core/database"
+import { ConflictError } from "@core/http"
+import { listAdminV1 } from "./listAdminV1"
+
+export async function checkAdminUsernameAvailabilityV1(
+	{ username }: { username: string },
+	existingSession?: DatabaseSession,
+): Promise<void> {
+	const session = createDatabaseSession(existingSession)
+	return await session.withTransaction(async () => {
+		const [[admin]] = await listAdminV1({ filter: { username } }, session)
+		if (admin) {
+			throw new ConflictError(`Admin with username: ${username} already exist`)
+		}
+	})
+}
