8a7eab2ea25702da112009b79bd7f1740ab88740
Kunal Gosrani <iamkunalgosrani@gmail.com>
2022-06-04 23:53:28 +0530

    test: category/update/v1 API

--- src/apis/Category/apiCategoryUpdateV1.ts
+++ src/apis/Category/apiCategoryUpdateV1.ts
@@ -6,24 +6,22 @@ import { updateCategoryV1 } from "@domain/Category/updateCategoryV1"
 import { parseYupSchema } from "apis/validators"
 import * as yup from "yup"
 
-const bodySchema = yup
-	.object({
-		filter: yup.object({ id: yup.string().uuid().required() }).required(),
-		update: yup.object({
-			title: yup.string().trim().max(100),
-			description: yup.string().trim().max(1000),
-		}),
-	})
-	.required()
+const bodySchema = yup.object({
+	filter: yup.object({ id: yup.string().uuid().required() }).required(),
+	update: yup.object({
+		title: yup.string().trim().max(100),
+		description: yup.string().trim().max(1000),
+	}),
+})
 
 export const apiCategoryUpdateV1 = new HttpApi({
 	endpoint: "/category/update/v1",
 	handler: async ({ req }) => {
 		authenticateRequest(req, [AuthRole.ADMIN])
-		const { filter, update } = await parseYupSchema(bodySchema, req.body)
-		const id = filter.id as CategoryId
+		const body = await parseYupSchema(bodySchema, req.body)
+		const id = body.filter.id as CategoryId
 
-		const category = await updateCategoryV1({ filter: { id }, update })
+		const category = await updateCategoryV1({ filter: { id }, update: body.update })
 		return serializeCategoryV1(category, AuthRole.ADMIN)
 	},
 })
