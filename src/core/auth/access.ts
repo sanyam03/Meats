16c15017e3fd19476ff85250687caa59c2fc84c5
Kunal Gosrani <iamkunalgosrani@gmail.com>
2022-06-04 16:53:10 +0530

    feat: api request authentication

---
+++ src/core/auth/access.ts
@@ -0,0 +1,26 @@
+import { ForbiddenError } from "@core/http"
+import { Request } from "express"
+import { AccessToken, validateAuthToken } from "./authToken"
+import { AuthRole } from "./types"
+
+function getAccessToken(authHeaderValue: string) {
+	const [_bearer, accessToken] = authHeaderValue.split(" ")
+	if (!_bearer || !accessToken) throw new ForbiddenError("Access token is required")
+	return accessToken as AccessToken
+}
+
+function getAuth(req: Request) {
+	const authHeaderValue = req.header("Authorization")
+	if (!authHeaderValue) throw new ForbiddenError("Authentication is required")
+
+	const accessToken = getAccessToken(authHeaderValue)
+	return validateAuthToken(accessToken)
+}
+
+export function authenticateRequest(req: Request, allowedRoles?: AuthRole[]) {
+	const auth = getAuth(req)
+	if (allowedRoles && !allowedRoles.includes(auth.role)) {
+		throw new ForbiddenError(`${auth.role} does not have access for this operation`)
+	}
+	return auth
+}
--- src/core/auth/types.ts
+++ src/core/auth/types.ts
@@ -1,8 +1,15 @@
 import { AdminId } from "@domain/Admin/Admin.entity"
 
 export enum AuthRole {
+	DEVELOPER = "Developer",
 	ADMIN = "Admin",
+	GUEST = "Guest",
 	USER = "User",
+}
+
+interface DeveloperAuthPayload {
+	role: AuthRole.DEVELOPER
+	id: string // use developer username
 }
 
 interface AdminAuthPayload {
@@ -10,4 +17,18 @@ interface AdminAuthPayload {
 	id: AdminId
 }
 
-export type AuthPayload = AdminAuthPayload
+interface GuestAuthPayload {
+	role: AuthRole.GUEST
+	id: string // client generated uuid
+}
+
+interface UserAuthPayload {
+	role: AuthRole.USER
+	id: string // user session id
+}
+
+export type AuthPayload =
+	| DeveloperAuthPayload
+	| AdminAuthPayload
+	| GuestAuthPayload
+	| UserAuthPayload
